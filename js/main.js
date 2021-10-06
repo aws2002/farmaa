e7e5f05a307aceb004cb767514a327d040251ddc
Osama Husam <80280073+aws2002@users.noreply.github.com>
2021-10-06 07:54:46 +0300

    se comt

--- js/main.js
+++ js/main.js
@@ -395,7 +395,7 @@ const counters = document.querySelectorAll('.counter');
                     items:1,
                 },
                 600:{
-                    items:2,
+                    items:1,
                 },
                 1000:{
                     items:3,
